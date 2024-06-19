fcdf2a2d503bb9f653e00d73b2b3d487581f72f8
Collekta <festus233@gmail.com>
2024-06-19 09:50:43 +0000

    Created a basic_operations.py script to perform basic arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#basic_operations.py
+
+#Assign values to variables
+number1 = 10
+number2 = 5
+
+#Perform the basic arithmetic operations
+sum_results = number1 + number2
+difference_results = number1 - number2
+product_results = number1 * number2
+
+#Print results
+print(f"Addition of {number1} and {number2} is {sum_results}")
+print(f"Subtraction of {number1} and {number2} is {difference_results}")
+print(f"Multiplication of {number1} and {number2} is {product_results}")
