7419c32c4f073d4233bd940aced2d34a8574e730
thecollekta <festus233@gmail.com>
2024-06-27 15:19:09 +0000

    Created a daily_reminder.py script to provide a customised reminder for a task.

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,23 @@
+# daily_reminder.py
+# Prompt user for input for a single task
+task_variable = input("Enter your task: ")
+priority_variable = input("Priority (high/medium/low): ")
+time_bound_variable = input("Is it time-bound? (yes/no): ")
+
+# Process the task based on priority and time sensitivity
+print("\nReminder: ")
+match priority_variable:
+    case "high":
+        print(f"'{task_variable}' is a high priority task ", end="")
+    case "medium":
+        print(f"'{task_variable}' is a medium priority task ", end="")
+    case "low":
+        print(f"'{task_variable}' is a low priority task. ", end="")
+    case _:
+        print("Invalid priority level entered.")
+
+# Check if the task is time-bound
+if time_bound_variable == "yes":
+    print("that requires immediate attention today!")
+else:
+    print("Consider completing it when you have free time.")
