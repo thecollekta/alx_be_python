f97b48e31b32f87f85108587a43cbce06e38b295
thecollekta <festus233@gmail.com>
2024-06-27 13:13:29 +0000

    Created a pattern_drawings.py script to print a square pattern made of the input size .

---
+++ control-flow/pattern_drawings.py
@@ -0,0 +1,16 @@
+# pattern_drawings.py
+
+# Prompt user for input
+size = int(input('Enter the size of the pattern: '))
+
+row = 0
+
+# Use while loop to iterate each row
+while row < size:
+    # Use a for loop to print asteriks in row
+    for _ in range(size):
+        print("*", end="")
+    # Print a newline character to move to next row
+    print()
+    # Move to the next row
+    row += 1
