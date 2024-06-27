f5acfb98faa4da65583c51b4dfb428640e792e12
thecollekta <festus233@gmail.com>
2024-06-27 09:32:29 +0000

    Created a weather_advice.py script for weather conditions

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,11 +1,12 @@
-#simple_interest.py
+# simple_interest.py
 
-#Variable definition
-principal = 1000 #Represents $1000
-rate = 0.05 #Represents 5% annual interest rate
-time = 3 #Represents 3 years
+# Variable definition
+principal = 1000 # Represents $1000
+rate = 0.05 # Represents 5% annual interest rate
+time = 3 # Represents 3 years
 
-#Calculate the simple interest
+# Calculate the simple interest
 interest = principal * rate * time
 
+# Print the calculated interest
 print(f"The simple interest is: {interest}")
